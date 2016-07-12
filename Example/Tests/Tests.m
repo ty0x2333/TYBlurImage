8e1092a0f4d594da1017c8fcc0847f6e3d24fb0e
luckytianyiyan <luckytianyiyan@gmail.com>
2016-07-12 23:18:29 +0800

    M remove tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -10,35 +10,35 @@
 
 SPEC_BEGIN(InitialTests)
 
-describe(@"My initial tests", ^{
+describe(@"TYBlurImage", ^{
 
-  context(@"will fail", ^{
-
-      it(@"can do maths", ^{
-          [[@1 should] equal:@2];
-      });
-
-      it(@"can read", ^{
-          [[@"number" should] equal:@"string"];
-      });
-    
-      it(@"will wait and fail", ^{
-          NSObject *object = [[NSObject alloc] init];
-          [[expectFutureValue(object) shouldEventually] receive:@selector(autoContentAccessingProxy)];
-      });
-  });
-
-  context(@"will pass", ^{
-    
-      it(@"can do maths", ^{
-        [[@1 should] beLessThan:@23];
-      });
-    
-      it(@"can read", ^{
-          [[@"team" shouldNot] containString:@"I"];
-      });  
-  });
-  
+//  context(@"will fail", ^{
+//
+//      it(@"can do maths", ^{
+//          [[@1 should] equal:@2];
+//      });
+//
+//      it(@"can read", ^{
+//          [[@"number" should] equal:@"string"];
+//      });
+//    
+//      it(@"will wait and fail", ^{
+//          NSObject *object = [[NSObject alloc] init];
+//          [[expectFutureValue(object) shouldEventually] receive:@selector(autoContentAccessingProxy)];
+//      });
+//  });
+//
+//  context(@"will pass", ^{
+//    
+//      it(@"can do maths", ^{
+//        [[@1 should] beLessThan:@23];
+//      });
+//    
+//      it(@"can read", ^{
+//          [[@"team" shouldNot] containString:@"I"];
+//      });  
+//  });
+//  
 });
 
 SPEC_END
